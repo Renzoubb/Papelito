1883faef37008743d96734419df77ad68c7758ba
rubeira <rubeira@midasconsultores.com.ar>
2022-10-27 11:09:31 -0300

    comentario codigo Mari

--- src/main/java/com/api/Api/Rest/controllers/UsuarioController.java
+++ src/main/java/com/api/Api/Rest/controllers/UsuarioController.java
@@ -14,50 +14,50 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "api/v1/usuarios")
 public class UsuarioController extends BaseControllerImpl<Usuario, UsuarioServiceImpl> {
 
-    private UsuarioService usuarioService;
-    public UsuarioController (UsuarioService usuarioService){
-       this.usuarioService = usuarioService;
-   }
-    @GetMapping("")
-    public ResponseEntity<?> MostrarUsuarios(){
-        try{
-            return ResponseEntity.status(HttpStatus.OK).body(usuarioService.MostrarUsuarios());
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error.Por favor intente mas tarde.\"}");
-        }
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> BuscarUsuarioPorId(@PathVariable Long id){
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(usuarioService.findById(id));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error.Por favor intente mas tarde.\"}");
-        }
-    }
-    @PostMapping("")
-    public ResponseEntity<?> CreateUsuario(@RequestBody Usuario entity){
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(usuarioService.save(entity));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error.Por favor intente mas tarde.\"}");
-        }
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> ActualizarUsuario(@PathVariable Long id,@RequestBody Usuario entity){
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(usuarioService.update(id,entity));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error.Por favor intente mas tarde.\"}");
-        }
-    }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> EliminarUsuario(@PathVariable Long id){
-        try {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(usuarioService.delete(id));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error.Por favor intente mas tarde.\"}");
-        }
-    }
+//    private UsuarioService usuarioService;
+//    public UsuarioController (UsuarioService usuarioService){
+//       this.usuarioService = usuarioService;
+//   }
+//    @GetMapping("")
+//    public ResponseEntity<?> MostrarUsuarios(){
+//        try{
+//            return ResponseEntity.status(HttpStatus.OK).body(usuarioService.MostrarUsuarios());
+//        }catch (Exception e){
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error.Por favor intente mas tarde.\"}");
+//        }
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<?> BuscarUsuarioPorId(@PathVariable Long id){
+//        try {
+//            return ResponseEntity.status(HttpStatus.OK).body(usuarioService.findById(id));
+//        }catch (Exception e){
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error.Por favor intente mas tarde.\"}");
+//        }
+//    }
+//    @PostMapping("")
+//    public ResponseEntity<?> CreateUsuario(@RequestBody Usuario entity){
+//        try {
+//            return ResponseEntity.status(HttpStatus.OK).body(usuarioService.save(entity));
+//        }catch (Exception e){
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error.Por favor intente mas tarde.\"}");
+//        }
+//    }
+//
+//    @PutMapping("/{id}")
+//    public ResponseEntity<?> ActualizarUsuario(@PathVariable Long id,@RequestBody Usuario entity){
+//        try {
+//            return ResponseEntity.status(HttpStatus.OK).body(usuarioService.update(id,entity));
+//        }catch (Exception e){
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error.Por favor intente mas tarde.\"}");
+//        }
+//    }
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<?> EliminarUsuario(@PathVariable Long id){
+//        try {
+//            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(usuarioService.delete(id));
+//        }catch (Exception e){
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error.Por favor intente mas tarde.\"}");
+//        }
+//    }
 }
