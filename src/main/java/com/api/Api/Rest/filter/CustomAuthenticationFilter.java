6b132a62537b849f92383c0cc47347559de09698
pao95 <paohinos1995@gmail.com>
2022-11-23 04:07:47 -0300

    arreglos

--- src/main/java/com/api/Api/Rest/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/api/Api/Rest/filter/CustomAuthenticationFilter.java
@@ -1,13 +1,9 @@
 package com.api.Api.Rest.filter;
 
-import com.api.Api.Rest.entities.Usuario;
-import com.api.Api.Rest.services.UsuarioService;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -33,8 +29,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
-    @Autowired
-    private  UsuarioService usuarioService;
 
     public CustomAuthenticationFilter(AuthenticationManager authenticationManager){
         this.authenticationManager = authenticationManager;
@@ -58,7 +52,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
-        Usuario usuario = usuarioService.getUsuario(user.getUsername());
+
         String refresh_Token = JWT.create()
                 .withSubject(user.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis()+ 30 * 60 * 1000))
@@ -70,7 +64,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         Map<String,String> tokens = new HashMap<>();
         tokens.put("access_token", access_Token);
         tokens.put("refresh_token", refresh_Token);
-        tokens.put("user_id", usuario.getId().toString());
+        tokens.put("user", user.getUsername());
 
         response.setContentType(APPLICATION_JSON_VALUE);
         new ObjectMapper().writeValue(response.getOutputStream(),tokens);
